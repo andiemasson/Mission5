988b5689024e292de626b7e6d7bea325752ee40e
andiemasson <aclegg1@byu.edu>
2025-02-07 16:03:00 -0700

    all the things

--- WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
+++ WebApplication3/obj/Debug/net8.0/WebApplication3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37bc938079f96c737007215ee57cfa686c7ff35b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
